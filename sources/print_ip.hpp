1f850e3be2c3706256c459bc6392bb24f4dc47e8
Alexey Abramov <alexpoligon@mail.ru>
2019-08-31 23:05:12 +0300

    add print ip from homogeneous tuple

--- sources/print_ip.hpp
+++ sources/print_ip.hpp
@@ -2,13 +2,14 @@
 
 #include <string>
 #include <vector>
+#include <tuple>
 #include <type_traits>
 
 // ------------------------------------------------------------------
 template<typename T>
 decltype
 (
-    std::declval<std::enable_if_t<std::is_integral<T>::value>>(),
+    std::enable_if_t<std::is_integral<T>::value>(),
     std::string()
 )
 ip_to_string(const T& addr) {
@@ -29,7 +30,7 @@ ip_to_string(const T& addr) {
 template<typename T>
 decltype
 ( 
-    std::declval<std::enable_if_t<std::is_same<std::string, T>::value>>(),
+    std::enable_if_t<std::is_same<std::string, T>::value>(),
     std::string()
 )
 ip_to_string(const T& addr) {
@@ -40,7 +41,7 @@ ip_to_string(const T& addr) {
 template<class T>
 decltype
 (
-    std::declval<std::enable_if_t<!std::is_same<std::string, T>::value>>(), 
+    std::enable_if_t<!std::is_same<std::string, T>::value>(), 
     std::declval<T>().cbegin(), 
     std::declval<T>().cend(), 
     std::string()
@@ -55,5 +56,52 @@ ip_to_string(const T& addr) {
             result += '.';
         }
     }
+    return result;
+}
+
+// ------------------------------------------------------------------
+template<typename... T>
+constexpr typename std::enable_if_t<sizeof...(T) < 2, bool>
+are_same() {
+    return true;
+}
+
+// ------------------------------------------------------------------
+template<typename A, typename B, typename... T>
+constexpr bool
+are_same() {
+    return std::is_same<A, B>::value && are_same<T...>();
+}
+
+// ------------------------------------------------------------------
+template<std::size_t I = 0, typename... T>
+decltype
+(
+    std::enable_if_t<are_same<T...>()>(),
+    std::enable_if_t<I == sizeof...(T)>(),
+    std::string()
+)
+ip_to_string(const std::tuple<T...>&) {
+    return "";
+}
+
+// ------------------------------------------------------------------
+template<std::size_t I = 0, typename... T>
+decltype
+(
+    std::enable_if_t<are_same<T...>()>(),
+    std::enable_if_t<I < sizeof...(T)>(),
+    std::string()
+)
+ip_to_string(const std::tuple<T...>& addr) {
+    std::string result;
+
+    result += std::to_string(std::get<I>(addr));
+
+    if (I < sizeof...(T)-1) {
+        result += '.';
+    }
+
+    result += ip_to_string<I+1, T...>(addr);
     return result;
 }
--- tests/print_ip_gtest.cpp
+++ tests/print_ip_gtest.cpp
@@ -3,9 +3,20 @@
 #include <vector>
 #include <list>
 #include <forward_list>
+#include <tuple>
 
 #include "print_ip.hpp"
 
+
+// ------------------------------------------------------------------
+TEST(IPtoStringTest, are_same_test) {
+    static_assert(are_same(),      "no type is not same");
+    static_assert(are_same<int>(), "one int type is not same" );
+    static_assert(are_same<int, int>(), "int, int types are not same" );
+    static_assert(are_same<int, int, int>(), "int, int, int types are not same" );
+    static_assert(!are_same<float, int>(), "float, int types are same" );
+    static_assert(!are_same<int, int, int, float>(), "int, int, int, float types are same" );
+}
 
 // ------------------------------------------------------------------
 TEST(IPtoStringTest, IPtoStringTest) {
@@ -21,7 +32,6 @@ TEST(IPtoStringTest, IPtoStringTest) {
     ASSERT_EQ(ip_to_string(true),  "1");
     ASSERT_EQ(ip_to_string(false), "0");
 
-
     ASSERT_EQ(ip_to_string<char>(-1),                  "255");
     ASSERT_EQ(ip_to_string<short>(0),                  "0.0");
     ASSERT_EQ(ip_to_string<int>(2130706433),           "127.0.0.1");
@@ -31,9 +41,7 @@ TEST(IPtoStringTest, IPtoStringTest) {
     ASSERT_EQ(ip_to_string(localhost), "127.0.0.1");
     
     ASSERT_EQ(ip_to_string(std::string{"127.0.0.1"}), "127.0.0.1");
-    
- // ASSERT_EQ(ip_to_string("127.0.0.1"), "127.0.0.1");
-    
+        
     std::vector<int> v_ip{127, 0, 0, 1};
     ASSERT_EQ(ip_to_string(v_ip), "127.0.0.1");
 
@@ -43,8 +51,11 @@ TEST(IPtoStringTest, IPtoStringTest) {
     std::forward_list<int> fl_ip{127, 0, 0, 1};
     ASSERT_EQ(ip_to_string(fl_ip), "127.0.0.1");
 
- // static_assert(std::is_integral<std::string>::value, "isn't integral");
+    std::tuple<int, int, int, int> t{127, 0, 0, 1};
+    ASSERT_EQ(ip_to_string(t), "127.0.0.1");
 
+ // std::tuple<int, int, int, float> t1{127, 0, 0, 1.5};
+ // ASSERT_EQ(ip_to_string(t1), "127.0.0.1");
 }
 
 // ------------------------------------------------------------------
